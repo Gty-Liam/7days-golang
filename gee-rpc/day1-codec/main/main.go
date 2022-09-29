9d9ae3348388a23d7b9d97666e8d4cc57b8f6f1d
gaotianyu <gaotianyu01@bilibili.com>
2022-09-29 13:33:48 +0800

    note

--- gee-rpc/day1-codec/main/main.go
+++ gee-rpc/day1-codec/main/main.go
@@ -25,15 +25,15 @@ func main() {
 	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
-	//time.Sleep(time.Second * 1)
+	time.Sleep(time.Second * 1)
 
 	// in fact, following code is like a simple geerpc client
 	log.Printf("clent: 尝试建立连接")
 	conn, _ := net.Dial("tcp", <-addr)
 	defer func() { _ = conn.Close() }()
 
-	//log.Printf("sleep...\n")
-	time.Sleep(time.Second * 3)
+	log.Printf("sleep...\n")
+	time.Sleep(time.Second)
 
 	// send options
 	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
@@ -48,15 +48,18 @@ func main() {
 		// head, body
 		log.Printf("写请求： header: %+v | body: %+v \n", h, fmt.Sprintf("geerpc req %d", h.Seq))
 		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq)) // 把h和body写到buf中
-		//time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 1)
 
-		var header *codec.Header
-		//time.Sleep(1000)
+		var header = &codec.Header{}
+		time.Sleep(1000)
 		log.Printf("处理完读resp ↓")
 		_ = cc.ReadHeader(header) // 从conn中读取到header
 		var reply string
 		_ = cc.ReadBody(&reply) // 从conn中读取body
+		log.Println("header:", header)
 		log.Println("reply:", reply)
+		log.Println("/***************/")
+		time.Sleep(1000)
 	}
 
 }
